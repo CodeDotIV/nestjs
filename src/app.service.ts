b67f86843b2de2e91fe1eeaee331496b6b61cde5
jeevan <jeevan@example.com>
2024-05-08 14:49:59 +0530

    adding new myapp

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World! welcome to jio eudiwouygfwalisnjcxlisu2984y3109reiwp9o7glius love u baby';
+  }
+}
